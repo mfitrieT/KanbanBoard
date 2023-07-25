e4a3938b4465a8c24ec1571b33161385b739317e
Fitrie TQ <m.fitrie@thunderquote.com>
2023-07-25 23:43:58 +0800

    test(CardTask): :test_tube: add multiple test

--- tests/unit/CardTask.spec.js
+++ tests/unit/CardTask.spec.js
@@ -1,11 +1,27 @@
 import { shallowMount } from '@vue/test-utils'
 import CardTaskVue from '@/components/CardTask.vue'
-import { BIconTrash, BIconClock } from 'bootstrap-vue';
+// import { BIconTrash, BIconClock } from 'bootstrap-vue';
 
-test('ensure it has child component', () => {
+describe("CardTask.vue", () => {
   const wrapper = shallowMount(CardTaskVue, {
-    stubs: ['b-card-text', 'b-card']
+    stubs: ['b-card-text', 'b-card', 'BIconTrash', 'BIconClock'],
+    propsData: {
+      DueDate: "11/12/23"
+    }
   });
-  expect(wrapper.findComponent(BIconTrash).exists()).toBe(true);
-  expect(wrapper.findComponent(BIconClock).exists()).toBe(true);
+
+  test('ensure it has child component', ()=>{
+    expect(wrapper.find('.colTasks__taskCard').exists()).toBe(true);
+  });
+  
+  test("ensure there's due date", ()=>{
+    expect(wrapper.find('.colTasks__cardDate').text()).toBe("Due 11/12/23");
+  })
+
+  test("emit the btn-delete", ()=>{
+    wrapper.find(".colTasks__iconDeleteCard").trigger('click')
+    expect(wrapper.emitted()['btn-delete']).toBeTruthy();
+  })
+
 });
+
